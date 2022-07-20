6a0c1aafd3bd3a52f7f49e5133b77f16609d9a51
Franklin Shera <fshera96@gmail.com>
2022-07-20 17:06:09 +0300

    IDoc and IDocFile Types

--- webviews/types.ts
+++ webviews/types.ts
@@ -2,3 +2,12 @@ export interface IpostMessage {
   command: string;
   value: any;
 }
+export interface IDoc {
+  version: string;
+  files: IDocFile[];
+}
+export interface IDocFile {
+  title: string;
+  filename: string;
+  link: string;
+}
